4e7619c6c1eed36e1d29c7882e423477edaf833d
Praful Bhandari <128509560+prafulbhandari07@users.noreply.github.com>
2023-12-20 03:45:27 +0530

    App

---
+++ src/context/AppReducer.js
@@ -0,0 +1,16 @@
+export default function reducer(state, action) {
+    switch (action.type) {
+        case 'DELETE_TRANSACTION':
+            return {
+                ...state,
+                transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
+            }
+        case 'ADD_TRANSACTION':
+            return {
+                ...state,
+                transactions: [...state.transactions, action.payload]
+            }
+        default:
+            return state;
+    }
+}
